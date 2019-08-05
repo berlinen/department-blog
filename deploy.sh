ba67633b31c22a96edab77c72dcf40415503d003
weibin <1292393108@qq.com>
2019-08-05 17:50:26 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,14 @@
-# 确保脚本抛出遇到的错误
-set -e
+#!/usr/bin/env sh
 
-# 生成静态文件
+set -e
 npm run build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+cd dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
 
-sudo git init
-sudo git add -A
-sudo git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-sudo git push -f git@github.com:berlinen/department-blog.git master:gh-pages
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:berlinen/department-blog.git master:gh-pages
 
 cd -
