b38c0ca9b01a7a83485c8aab3749bb50d4b9f306
weibin <1292393108@qq.com>
2019-08-05 17:46:02 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "部门博客",
   description: 'study programs and record life',
-  base: '/blog/',
+  base: '/department-blog/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
